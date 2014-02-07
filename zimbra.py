065a5fed606f69453dd94a8da65a36b7c16b5e96
Sylvain Soliman <Sylvain.Soliman@inria.fr>
2014-02-07 23:17:55 +0100

    starting to zimbrify back sieve

--- zimbra.py
+++ zimbra.py
@@ -11,7 +11,8 @@ from pythonzimbra.response_xml import ResponseXml
 from pythonzimbra.communication import Communication
 
 from sievelib.parser import Parser
-from sievelib.commands import ActionCommand, TestCommand, add_commands
+from sievelib.commands import ActionCommand, TestCommand, RequireCommand, \
+    IfCommand, HeaderCommand, add_commands
 
 
 def display_rule(rule):
@@ -123,19 +124,16 @@ def display_action(action):
     print('/* unknown action: ' + str(action) + ' */ keep;')
 
 
-def authenticate(url):
+def get_token(url):
     login = 'Sylvain.Soliman@inria.fr'
     passwd = getpass.getpass()
 
-    token = auth.authenticate(
+    return auth.authenticate(
         url,
         login,
         passwd,
-        use_password=True)
-
-    request = RequestXml()
-    request.set_auth_token(token)
-    return request
+        use_password=True
+    )
 
 
 class AddflagCommand(ActionCommand):
@@ -207,6 +205,62 @@ class DateCommand(TestCommand):
     ]
 
 
+def zimbrify_header(htest, index, negative=False):
+    h = {
+        u'index': index,
+        u'stringComparison': htest['match-type'][1:],
+        u'value': htest['key-list'][0][1:-1],
+        u'header': ','.join(map(lambda h: h[1:-1], htest['header-names']))
+    }
+    if negative:
+        h[u'negative'] = '1'
+    if 'comparator' in htest.arguments:
+        if htest['comparator']['extra_arg']['values'] == '"i;ascii-casemap"':
+            h[u'caseSensitive'] = '0'
+    return h
+
+
+def zimbrify_test(test):
+    tests = {
+        u'condition': test.name
+    }
+    for (index, t) in enumerate(test['tests']):
+        if isinstance(t, HeaderCommand):
+            tt = zimbrify_header(t, index + 1)
+            if 'headerTest' not in tests:
+                tests['headerTest'] = []
+            tests['headerTest'].append(tt)
+    actions = {}
+    return(tests, actions)
+
+
+def zimbrify(command_list):
+    name = 'undefined'
+    active = '1'
+    commands = []
+    for command in command_list:
+        if isinstance(command, RequireCommand):
+            pass
+        elif isinstance(command, SetCommand):
+            if command['name'] == '"name"':
+                name = command['value'][1:-1]
+            elif command['name'] == '"active"':
+                active = command['value'][1:-1]
+            else:
+                print('unknown variable: ' + command['name'], file=sys.stderr)
+        elif isinstance(command, IfCommand):
+            (tests, actions) = zimbrify_test(command['test'])
+            cmd = {
+                u'name': name, u'active': active, u'filterTests': tests,
+                u'filterActions': actions
+            }
+            commands.append(cmd)
+        else:
+            print('unknown command: ' + command, file=sys.stderr)
+    commands.reverse()
+    return commands
+
+
 def parse():
     print('parsing ' + sys.argv[1])
     add_commands([AddflagCommand, SetCommand, TagCommand, DateCommand])
@@ -214,14 +268,17 @@ def parse():
     if p.parse_file(sys.argv[1]) is False:
         print(p.error)
     else:
-        p.dump()
+        print(zimbrify(p.result))
 
 
 def main():
     url = 'https://zimbra.inria.fr/service/soap/'
-    request = authenticate(url)
 
     if len(sys.argv) < 2:
+        token = get_token(url)
+        request = RequestXml()
+        request.set_auth_token(token)
+
         request.add_request('GetFilterRulesRequest', {}, 'urn:zimbraMail')
 
         response = ResponseXml()
@@ -234,6 +291,7 @@ def main():
                   ' "imap4flags", "body", "variables"];')
             print()
             for rule in rules['filterRules']['filterRule']:
+                print(rule)
                 display_rule(rule)
                 print()
     else:
