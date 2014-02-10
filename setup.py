9df02e26d6397c7c907d6b01d371c1a9177980fa
Sylvain Soliman <Sylvain.Soliman@inria.fr>
2014-02-10 20:53:09 +0100

    almost packaged, except for README
    --HG--
    rename : README.rst => README.txt

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup
 
-with open('README.rst') as f:
+with open('README.txt') as f:
     long_descr = f.read()
 
 setup(
@@ -8,12 +9,12 @@ setup(
     version='1.0.0',
     author='Sylvain Soliman',
     author_email='Sylvain.Soliman@inria.fr',
+    url='http://lifeware.inria.fr/~soliman',
     description='Partial Zimbra filters to/from sieve text files converter',
-    packages=['zimbratosthenes'],
-    package_dir={'zimbratosthenes': ''},
+    py_modules=['zimbra'],
     entry_points={
         'console_scripts': [
-            'zbt = zimbratosthenes.zimbra:main',
+            'zbt = zimbra:main',
         ],
     },
     license='MIT',
@@ -21,5 +22,15 @@ setup(
     install_requires=[
         'sievelib >= 0.8',
         'python-zimbra >= 1.0-rc5',
-    ]
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='zimbra mail sieve filters',
 )
