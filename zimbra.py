a96021560208970d84a9f46846509843a5e0f867
Sylvain Soliman <Sylvain.Soliman@inria.fr>
2014-02-11 23:28:07 +0100

    dooooocs

--- zimbra.py
+++ zimbra.py
@@ -20,6 +20,10 @@ from sievelib.commands import ActionCommand, TestCommand, RequireCommand, \
 
 
 def display_rule(rule):
+    '''print out one single Zimbra filter
+
+    use two varibles to store name and active flag, then one test with
+    possibly many actions'''
     print(u'set "name" "' + rule[u'name'] + u'";')
     print(u'set "active" "' + rule[u'active'] + u'";')
     print(u'if ', end=u'')
@@ -30,12 +34,17 @@ def display_rule(rule):
 
 
 def display_test(test):
+    '''any Zimbra filter is anyof/allof and then possibly many tests'''
     print(test[u'condition'] + u' (')
     print(u',\n'.join(transform_tests(test)))
     print(u') ', end=u'')
 
 
 def transform_tests(tests):
+    '''for each subtest category, convert the tests to a single list and print
+
+    tests are grouped by category in Zimbra but not in Sieve. The index is
+    used for ordering'''
     new_tests = []
     known_tests = [u'headerTest', u'sizeTest', u'dateTest', u'bodyTest',
                    u'headerExistsTest']
@@ -60,6 +69,7 @@ def transform_tests(tests):
 
 
 def translate(category, key):
+    '''Utility function for Zimbra to Sieve translation'''
     dic = {
         u'date': {u'before': u'le', u'after': u'ge'},
         u'flag': {u'read': u'\\\\Seen', u'flagged': u'\\\\Flagged'}
@@ -68,6 +78,7 @@ def translate(category, key):
 
 
 def show_test(test):
+    '''return a Sieve string for a single test'''
     show = u'   '
     if test.get(u'negative') == u'1':
         show += u'not '
@@ -113,6 +124,7 @@ def show_test(test):
 
 
 def display_actions(actions):
+    '''display a list of actions in the order specified by their index'''
     a = actions.items()
     a.sort(key=lambda (_, x): int(x.get(u'index')))
     for action in a:
@@ -121,6 +133,7 @@ def display_actions(actions):
 
 
 def display_action(action):
+    '''print the Sieve string for a single action'''
     if action[0] == u'actionFileInto':
         print(u'fileinto "' + action[1][u'folderPath'] + u'";')
         return
@@ -150,6 +163,7 @@ def display_action(action):
 
 
 def get_token(url):
+    '''Get authentication token from Zimbra SOAP API'''
     login = u'Sylvain.Soliman@inria.fr'
     passwd = getpass.getpass()
 
@@ -162,6 +176,9 @@ def get_token(url):
 
 
 class AddflagCommand(ActionCommand):
+    '''Sieve command to handle Zimbra flags'''
+    # extension_map is hardcoded in sievelib, so we cannot use this
+    # is_extension = True
     args_definition = [
         {
             "name": "flag",
@@ -172,6 +189,8 @@ class AddflagCommand(ActionCommand):
 
 
 class SetCommand(ActionCommand):
+    '''Sieve command to handle variables used to store Zimbra rule name'''
+    is_extension = True
     args_definition = [
         {
             "name": "name",
@@ -187,6 +206,9 @@ class SetCommand(ActionCommand):
 
 
 class TagCommand(ActionCommand):
+    '''Sieve command to handle Zimbra tags'''
+    # extension_map is hardcoded in sievelib, so we cannot use this
+    # is_extension = True
     args_definition = [
         {
             "name": "tag",
@@ -197,7 +219,9 @@ class TagCommand(ActionCommand):
 
 
 class DateCommand(TestCommand):
-    is_extension = True
+    '''Sieve test for Zimbra date comparisons'''
+    # extension_map is hardcoded in sievelib, so we cannot use this
+    # is_extension = True
     args_definition = [
         {
             "name": "zone",
@@ -231,6 +255,8 @@ class DateCommand(TestCommand):
 
 
 class BodyCommand(TestCommand):
+    '''Sieve test for Zimbra body matches'''
+    is_extension = True
     args_definition = [
         comparator,
         match_type,
@@ -241,6 +267,7 @@ class BodyCommand(TestCommand):
 
 
 def zimbrify_header(htest):
+    '''Return a Zimbra headerTest for the corresponding Sieve test'''
     h = {
         u'stringComparison': unicode(htest[u'match-type'][1:]),
         u'value': unicode(htest[u'key-list'][0][1:-1]),
@@ -254,6 +281,7 @@ def zimbrify_header(htest):
 
 
 def zimbrify_address(htest):
+    '''Return a Zimbra addressTest for the corresponding Sieve test'''
     h = zimbrify_header(htest)
     if u'address_part' in htest.arguments:
         h[u'part'] = unicode(htest[u'address_part'][1:])
@@ -261,6 +289,7 @@ def zimbrify_address(htest):
 
 
 def zimbrify_size(htest):
+    '''Return a Zimbra sizeTest for the corresponding Sieve test'''
     limit = int(htest[u'limit'])
     units = [u'B', u'K', u'M', u'G']
     idx = 0
@@ -276,11 +305,13 @@ def zimbrify_size(htest):
 
 
 def zimbrify_exist(htest):
+    '''Return a Zimbra headerExistsTest for the corresponding Sieve test'''
     return {u'header': unicode(htest[u'header-names'][0][1:-1])}
 
 
 def zimbrify_body(htest):
-    # deliberately ignoring the case where we get a list
+    '''Return a Zimbra bodyTest for the corresponding Sieve test'''
+    # FIXME deliberately ignoring the case where we get a list
     h = {u'value': unicode(htest[u'key-list'][1:-1])}
     if u'comparator' in htest.arguments:
         if htest[u'comparator'][u'extra_arg'] == u'"i;ascii-casemap"':
@@ -289,6 +320,7 @@ def zimbrify_body(htest):
 
 
 def zimbrify_date(htest):
+    '''Return a Zimbra dateTest for the corresponding Sieve test'''
     if htest[u'comparison'] == u'"le"':
         comp = u'before'
     else:
@@ -304,6 +336,7 @@ def zimbrify_date(htest):
 
 
 def zimbrify_actions(actions):
+    '''Return a dict of Zimbra actions for the corresponding Sieve actions'''
     acts = {}
     for (index, a) in enumerate(actions):
         cat = None
@@ -335,6 +368,7 @@ def zimbrify_actions(actions):
 
         if cat is not None:
             aa[u'index'] = unicode(index)
+            # use a single value, then a list
             if cat not in acts:
                 acts[cat] = aa
             elif not isinstance(acts[cat], list):
@@ -345,6 +379,7 @@ def zimbrify_actions(actions):
 
 
 def zimbrify_test(test):
+    '''Return a dict of Zimbra tests for the corresponding Sieve tests'''
     tests = {
         u'condition': unicode(test.name)
     }
@@ -378,6 +413,7 @@ def zimbrify_test(test):
             if negative:
                 tt[u'negative'] = u'1'
             tt[u'index'] = unicode(index)
+            # use a single value, then a list
             if cat not in tests:
                 tests[cat] = tt
             elif not isinstance(tests[cat], list):
@@ -388,6 +424,7 @@ def zimbrify_test(test):
 
 
 def zimbrify(command_list):
+    '''Translate a parsed sieve file to a Zimbra filter list'''
     name = u'undefined'
     active = u'1'
     commands = []
@@ -416,12 +453,14 @@ def zimbrify(command_list):
 
 
 def init_parser():
+    '''initialize a Sieve parser with our supplementary commands'''
     add_commands([AddflagCommand, SetCommand, TagCommand, DateCommand,
                   BodyCommand])
     return Parser()
 
 
 def parse():
+    '''parse either a file or stdin and convert the result to Zimbra format'''
     if sys.argv[1] == u'-':
         inputfile = sys.stdin
     else:
@@ -435,6 +474,7 @@ def parse():
 
 
 def display_rules(rules):
+    '''print out a Sieve file corresponding to Zimbra filter rules'''
     print(u'require ["date", "relational", "fileinto",' +
           u' "imap4flags", "body", "variables"];')
     print(u'')
@@ -444,6 +484,9 @@ def display_rules(rules):
 
 
 def update_rules(comm, token, rules):
+    '''If confirmed try to upload rules corresponding to a parse
+
+    if there is an issue, try to re-upload original rules'''
     new_rules = {u'filterRules': {u'filterRule': parse()}}
     confirm = raw_input(u'Do you wish to proceed [y/N]? ')
     if not confirm[0] in [u'y', u'Y']:
@@ -466,6 +509,7 @@ def update_rules(comm, token, rules):
 
 
 def communicate(comm, token, request_type, request_args):
+    '''Send a request to Zimbra SOAP API and return response'''
     request = RequestXml()
     request.set_auth_token(token)
     request.add_request(request_type, request_args, u'urn:zimbraMail')
@@ -476,6 +520,7 @@ def communicate(comm, token, request_type, request_args):
 
 
 def usage():
+    '''Command usage'''
     print(u'''Usage:
   {0} [file.sieve]
 
@@ -490,6 +535,7 @@ Zimbra server and convert them to sieve rules, displayed on standard output.
 
 
 def main():
+    '''Test arguments and either convert Zimbra to Sieve or the opposite'''
     if len(sys.argv) > 2 or u'-h' in sys.argv or u'--help' in sys.argv:
         usage()
 
